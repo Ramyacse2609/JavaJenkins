c18c248c352bfc08e9d2b7738625554549ed4aca
Ramyacse2609 <ramyasriv1995@outlook.com>
2019-02-15 12:25:57 +0530

    add the calculator java file

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+	public static void main(String args[])
+	{
+
+	Calculator obj=new Calculator();
+	System.out.println(obj.add(5,6));
+	System.out.println(obj.sub(7,5));
+}
+
+	public int add(int a,int b)
+{
+	return a+b;
+}
+	public int sub(int a,int b)
+{
+	return a-b;
+	
+}
+}
